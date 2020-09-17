7d7b8ea56e7d048877011045307385dd6de60831
Soldy <4786022+Soldy@users.noreply.github.com>
2020-09-17 22:06:28 +0100

    test with 100% code coverage
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ test.js
@@ -0,0 +1,113 @@
+
+const nanoTest  = new (require('nanoTest')).test({
+    'debugPrint' : 'short'
+});
+const sandboxClass = require('./index.js').sandboxClass;
+
+
+nanoTest.add(
+    'check simple ',
+    {
+        'function':async ()=>{
+            let sandbox = new sandboxClass({
+                'name'     : 'top',
+                'test'     : {
+                    'function' : function(){
+                       return 'two2';
+                    },
+                    'options'  :[
+                       'two2'
+                    ]
+                }
+            });
+            let out = await sandbox.check();
+            return out.value;
+        }
+    },
+    '===',
+    'two2'
+);
+nanoTest.add(
+    'check error',
+    {
+        'function':async ()=>{
+            let sandbox = new sandboxClass({
+                'name'     : 'top',
+                'test'     : {
+                    'function' : ()=>{
+                        nofunction();
+                    },
+                    'options'  :[]
+                },
+            });
+            let out = await sandbox.check();
+            return out.result;
+        }
+    },
+    '===',
+    3
+);
+nanoTest.add(
+    'check undefined test',
+    {
+        'function':async ()=>{
+            let sandbox = new sandboxClass({
+                'name'     : 'top',
+            });
+            let out = await sandbox.check();
+            return out.result;
+        }
+    },
+    '===',
+    4
+);
+nanoTest.add(
+    'check undefined function',
+    {
+        'function':async ()=>{
+            let sandbox = new sandboxClass({
+                'name'     : 'top',
+                'test'     : {
+                    'options'  :[]
+                }
+            });
+            let out = await sandbox.check();
+            return out.result;
+        }
+    },
+    '===',
+    4
+);
+nanoTest.add(
+    'check String',
+    {
+        'function':async ()=>{
+            let sandbox = new sandboxClass({
+                'name'     : 'top',
+                'test'     :  '(()=>{ return "string";})()',
+            });
+            let out = await sandbox.check();
+            return out.value;
+        }
+    },
+    '===',
+    'string'
+);
+nanoTest.add(
+    'check Legacy',
+    {
+        'function':async ()=>{
+            let sandbox = new sandboxClass({
+                'name'     : 'top',
+                'test'     : function(){
+                       return 'Legacy';
+                },
+            });
+            let out = await sandbox.check();
+            return out.value;
+        }
+    },
+    '===',
+    'Legacy'
+);
+nanoTest.run();
